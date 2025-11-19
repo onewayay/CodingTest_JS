af66c9f3812f198494ac4f9862890a05f40bbaa6
Lim Han Gil <108395740+onewayay@users.noreply.github.com>
2025-11-19 11:13:50 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.12 ms, Memory: 33.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let answer = [n];
+    let num = n;
+    do {
+        if(num % 2 === 0){
+            num = num / 2;
+        } else {
+            num = 3 * num + 1;
+        }
+        answer.push(num);
+    } while (num !== 1);
+    return answer;
+}
