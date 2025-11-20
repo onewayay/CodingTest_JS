c48a967c085b3c316f875a905ae4265e4e9c772f
Lim Han Gil <108395740+onewayay@users.noreply.github.com>
2025-11-20 16:05:15 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -0,0 +1,9 @@
+function solution(my_strings, parts) {
+    let answer = '';
+    
+    parts.forEach(([s, e], idx)=>{
+        answer += my_strings[idx].slice(s, e+1);
+    })
+    
+    return answer;
+}
