5ca9640cc6e44b98cba691a8ea15e5bf4cbab4b1
Lim Han Gil <108395740+onewayay@users.noreply.github.com>
2025-11-23 21:26:09 +0900

    [level 0] Title: 리스트 자르기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,19 @@
+function solution(n, slicer, num_list) {
+    let answer = [];
+    
+    let [a, b, c] = [...slicer];
+    switch(n){
+        case 1: answer = num_list.slice(0, b + 1);
+            break;
+        case 2: answer = num_list.slice(a);
+            break;
+        case 3: answer = num_list.slice(a, b + 1);
+            break;
+        case 4: for(let i = a; i <= b; i += c){
+            answer.push(num_list[i])
+        }
+            break;
+    }
+    
+    return answer;
+}
