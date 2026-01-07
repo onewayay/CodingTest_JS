9650b9f3763e6a451e8242eb4d1f28c99369d084
Lim Han Gil <108395740+onewayay@users.noreply.github.com>
2026-01-07 15:13:58 +0900

    [level 1] Title: 달리기 경주, Time: 221.18 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,17 +1,17 @@
 function solution(players, callings) {
-    const pos = {};
+    const rank = {};
     
-    players.forEach((p, i) => pos[p] = i);
+    players.forEach((p, i) => rank[p] = i);
 
     for (const name of callings) {
-        const i = pos[name];
+        const i = rank[name];
         const front = players[i - 1];
 
         players[i - 1] = name;
         players[i] = front;
 
-        pos[name] = i - 1;
-        pos[front] = i;
+        rank[name] = i - 1;
+        rank[front] = i;
     }
 
     return players;
