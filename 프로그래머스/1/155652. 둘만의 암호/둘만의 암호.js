8482c1f862382cd253f64ffa1b30e30f6cfd1102
Lim Han Gil <108395740+onewayay@users.noreply.github.com>
2026-01-14 21:59:07 +0900

    [level 1] Title: 둘만의 암호, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,29 @@
+function solution(s, skip, index) {
+    // 알파벳 배열
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    
+    let answer = '';
+    
+    for(let i = 0; i < s.length; i++){
+        // 현재 알파벳이 전체 알파벳에서 몇번째 순서인지
+        let sIndex = alphabet.indexOf(s[i]);
+        
+        // 알파벳 이동할 숫자
+        let count = 0;
+        
+        // 0 ~ count 반복
+        while (count < index) {     
+            // z 넘어가면 a로 다시 돌아오도록
+            sIndex = (sIndex + 1) % 26;
+
+            // skip에 해당 없으면 count 증가, 해당 없으면 count 증가 안하고 다시 반복
+            if (!skip.includes(alphabet[sIndex])) {
+                count++;
+            }
+        }
+
+        answer += alphabet[sIndex];
+    }
+    
+    return answer;
+}
