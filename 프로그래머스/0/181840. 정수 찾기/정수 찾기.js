8f9452437fc600273f65dfc210d39584d984fe9f
Lim Han Gil <108395740+onewayay@users.noreply.github.com>
2026-01-27 16:34:52 +0900

    [level 0] Title: 정수 찾기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.js
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.js
@@ -1,11 +1,3 @@
 function solution(num_list, n) {
-    let answer = 0;
-    
-    for(let num of num_list){
-        if(num === n){
-            answer = 1;
-        }
-    }
-    
-    return answer;
+    return num_list.includes(n) ? 1 : 0;
 }
