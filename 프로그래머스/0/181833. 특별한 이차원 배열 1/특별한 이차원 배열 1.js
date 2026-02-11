7e1e3bebdd9ceb9f6ad25efa92336c608f35f340
Lim Han Gil <108395740+onewayay@users.noreply.github.com>
2026-02-11 16:24:08 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.74 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let answer = [];
+    
+    for(let i = 0; i < n; i++){
+        answer.push([]);
+        
+        for(let j = 0; j < n; j++){
+            answer[i].push(i === j ? 1 : 0);
+        }
+    }
+    return answer;
+}
