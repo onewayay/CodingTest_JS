28019b648d126912c2903a0a7bab5aecd55d85e9
Lim Han Gil <108395740+onewayay@users.noreply.github.com>
2026-03-07 13:27:09 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,14 @@
+function solution(my_string) {
+    const arr = my_string.split(" ");
+    let answer = Number(arr[0]);
+    
+    for(let i = 1; i < arr.length; i += 2){
+        if(arr[i] === '+'){
+            answer += Number(arr[i+1]);
+        } else {
+            answer -= Number(arr[i+1]);
+        }
+    }
+    
+    return answer;
+}
