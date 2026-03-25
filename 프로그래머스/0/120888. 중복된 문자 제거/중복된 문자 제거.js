ae6a858fc24d4a5bb0982723002e67ea06814a14
Lim Han Gil <108395740+onewayay@users.noreply.github.com>
2026-03-25 17:09:49 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    let answer = '';
+
+    for (let char of my_string) {
+        if (!answer.includes(char)) {
+            answer += char;
+        }
+    }
+
+    return answer;
+}
