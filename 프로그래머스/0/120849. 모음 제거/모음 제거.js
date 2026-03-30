34f699fc61d5b6b5aea542bc53469ce291ba7c0b
Lim Han Gil <108395740+onewayay@users.noreply.github.com>
2026-03-30 21:13:26 +0900

    [level 0] Title: 모음 제거, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    let answer = '';
+    const vowel = ['a', 'e' ,'i' ,'o', 'u']
+    for(let i =0; i < my_string.length; i++){
+        if(!vowel.includes(my_string[i])){
+            answer += my_string[i];
+        }
+    }
+    
+    return answer;
+}
