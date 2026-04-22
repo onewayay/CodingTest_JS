ae128d89e001bda8ca897c589321fe9f9caed02f
Lim Han Gil <108395740+onewayay@users.noreply.github.com>
2026-04-22 17:17:34 +0900

    [level 1] Title: 햄버거 만들기, Time: 64.08 ms, Memory: 97.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,20 @@
+function solution(ingredient) {
+    let count = 0;
+    const stack = [];
+    
+    for(let item of ingredient){
+        stack.push(item);
+        
+        const len = stack.length;
+        if(len >= 4 &&
+           stack[len-4] === 1 &&
+           stack[len-3] === 2 &&
+           stack[len-2] === 3 &&
+           stack[len-1] === 1){
+            stack.splice(len - 4, 4);
+            count++;
+        }
+    }
+    
+    return count;
+}
